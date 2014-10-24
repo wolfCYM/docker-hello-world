07d550c699047f552ff7a9624aa34acca2d6aa0b
Ran Cheng <rancheng@mathildetech.coom>
2014-10-24 18:37:49 +0800

    test check-in3

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 	</style>
 </head>
 <body>
-    <h1> TEST </h1>
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
