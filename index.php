c52cce9a97faf52d88754f1bbd6562102fc6deaa
Yue Zuo <root@Yues-MacBook-Pro.local>
2014-10-24 18:23:57 +0800

    test check-in

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
